313be466790db680c5832c452943ddfc9efad82e
EngMo974 <42417845+EngMo974@users.noreply.github.com>
2020-05-09 03:21:02 +0300

    empty make design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,6 +7,80 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Question(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Question extends StatefulWidget {
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'Are You Crazy.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: FlatButton(
+              child: Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              color: Colors.green,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: FlatButton(
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              color: Colors.red,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        ),
+
+
+      ],
+    );
   }
 }
